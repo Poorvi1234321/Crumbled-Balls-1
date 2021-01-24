6443d2371f8fd656ae338e4606802bee9074e2a1
Poorvi1234321 <74653455+Poorvi1234321@users.noreply.github.com>
2021-01-24 18:17:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,47 +1,36 @@
-var log1,log2,log3;
-var ground;
-var paper;
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-
+function preload()
+{
+	
+}
 
 function setup() {
-  rectMode(CENTER);
-	createCanvas(1200, 600);
+	createCanvas(800, 700);
 
-  engine = Engine.create();
+
+	engine = Engine.create();
 	world = engine.world;
 
-  ground = new Ground(600,596,1400,20);
-  log1=new Dustbin(1000,570,200,20);
-	log2=new Dustbin(1090,510,20,100);
-  log3=new Dustbin(910,510,20,100);
-  paper=new  Paper(80,450);
-  console.log(paper)
+	//Create the Bodies Here.
+
+
 	Engine.run(engine);
+  
 }
 
 
 function draw() {
   rectMode(CENTER);
-  background("lightblue");
-  Engine.update(engine);
+  background(0);
   
-  log1.display();
-  log2.display();
-  log3.display();
-  ground.display();
-  paper.display();
-
   drawSprites();
-
+ 
 }
 
-function keyPressed() {
-if (keyCode=== UP_ARROW) {
-  Matter.Body.applyForce(paper.body,paper.body.position,{x:80,y:-85});
-}
-}
+
+
